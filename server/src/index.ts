6bb111e9c961385a077db9618d3247a64f096765
Michael McIsaac <55292798+mamcisaac@users.noreply.github.com>
2025-06-07 00:35:19 -0300

    ⚙️ Phase 0 tooling tweak: workspace scripts

--- server/src/index.ts
+++ server/src/index.ts
@@ -16,12 +16,12 @@ app.use((req, res) => {
   res.status(404).json({ error: 'Not Found' });
 });
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use(
   (
     err: Error,
     req: express.Request,
     res: express.Response,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     _next: express.NextFunction,
   ) => {
     console.error(err);
