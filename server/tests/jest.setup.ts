91f7dff40c85879da7d7705bc4bc13f4ea320663
Michael McIsaac <55292798+mamcisaac@users.noreply.github.com>
2025-06-07 23:57:27 -0300

    docs: instruct how to configure test env

--- server/tests/jest.setup.ts
+++ server/tests/jest.setup.ts
@@ -1,5 +1,12 @@
 import { config } from 'dotenv';
+import fs from 'fs';
 
 export default async () => {
-  config({ path: './.env.test' });
+  const testEnvPath = './.env.test';
+  const offlineEnvPath = './.env.offline';
+  if (fs.existsSync(testEnvPath)) {
+    config({ path: testEnvPath });
+  } else if (fs.existsSync(offlineEnvPath)) {
+    config({ path: offlineEnvPath });
+  }
 };
