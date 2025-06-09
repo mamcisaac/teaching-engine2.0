848bed95d097331d63003b081d543e67b534fd98
Michael McIsaac <55292798+mamcisaac@users.noreply.github.com>
2025-06-09 01:03:52 -0300

    test: remove console log from db setup

--- server/tests/setup-test-db.ts
+++ server/tests/setup-test-db.ts
@@ -4,7 +4,6 @@ import { resolve } from 'path';
 
 export default async function setup() {
   await envSetup();
-  console.log('Setting up test database...');
   const root = resolve(__dirname, '..', '..');
   execSync('pnpm db:generate', { stdio: 'inherit', cwd: root });
   execSync('pnpm db:deploy', { stdio: 'inherit', cwd: root });
